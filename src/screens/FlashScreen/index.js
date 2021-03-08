45d6f128345bd8444e66fc58d755127025639fa5
uv25 <=>
2021-03-08 10:26:55 +0530

    Saga with props

--- src/screens/FlashScreen/index.js
+++ src/screens/FlashScreen/index.js
@@ -1,5 +1,6 @@
-import React, {useCallback, useEffect} from 'react'
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+import React, { useState, useEffect, useCallback } from 'react'
+import {AppRegistry, View, Text, StyleSheet, TouchableOpacity, Button} from 'react-native'
+import ShareMenu, { ShareMenuReactView } from "react-native-share-menu";
 
 import { useDispatch, useSelector } from "react-redux";
 import rootReducer from "../../reducers";
@@ -12,6 +13,128 @@ const buttonAction = () => {
     dispatch(actions.userActions.setUser({name: 'Adi'}))
   )
 }
+
+type SharedItem = {
+  mimeType: string,
+  data: string,
+  extraData: any,
+};
+
+const Test = () => {
+  const [sharedData, setSharedData] = useState(null);
+  const [sharedMimeType, setSharedMimeType] = useState(null);
+
+  const handleShare = useCallback((item: ?SharedItem) => {
+    if (!item) {
+      return;
+    }
+
+    const { mimeType, data, extraData } = item;
+
+    setSharedData(data);
+    setSharedMimeType(mimeType);
+    // You can receive extra data from your custom Share View
+    console.log(extraData);
+  }, []);
+
+  useEffect(() => {
+    ShareMenu.getInitialShare(handleShare);
+    console.log("Loaded Test...");
+  }, []);
+
+  useEffect(() => {
+    const listener = ShareMenu.addNewShareListener(handleShare);
+    console.log("Loaded Test...");
+
+    return () => {
+      listener.remove();
+    };
+  }, []);
+
+  if (!sharedMimeType && !sharedData) {
+    // The user hasn't shared anything yet
+    return null;
+  }
+
+  if (sharedMimeType === "text/plain") {
+    // The user shared text
+    return <Text>Shared text: {sharedData}</Text>;
+  }
+
+  if (sharedMimeType.startsWith("image/")) {
+    // The user shared an image
+    return (
+      <View>
+        <Text>Shared image:</Text>
+        <Image source={{ uri: sharedData }} />
+      </View>
+    );
+  }
+
+  // The user shared a file in general
+  return (
+    <View>
+      <Text>Shared mime type: {sharedMimeType}</Text>
+      <Text>Shared file location: {sharedData}</Text>
+    </View>
+  );
+};
+
+const Share = () => {
+  const [sharedData, setSharedData] = useState("");
+  const [sharedMimeType, setSharedMimeType] = useState("");
+
+  useEffect(() => {
+    ShareMenuReactView.data().then(({ mimeType, data }) => {
+      setSharedData(data);
+      setSharedMimeType(mimeType);
+      console.log("Loaded Share...");
+    });
+  }, []);
+
+  return (
+    <View>
+      <Button
+        title="Dismiss"
+        onPress={() => {
+          ShareMenuReactView.dismissExtension();
+        }}
+      />
+      <Button
+        title="Send"
+        onPress={() => {
+          // Share something before dismissing
+          ShareMenuReactView.dismissExtension();
+        }}
+      />
+      <Button
+        title="Dismiss with Error"
+        onPress={() => {
+          ShareMenuReactView.dismissExtension("Something went wrong!");
+        }}
+      />
+      <Button
+        title="Continue In App"
+        onPress={() => {
+          ShareMenuReactView.continueInApp();
+        }}
+      />
+      <Button
+        title="Continue In App With Extra Data"
+        onPress={() => {
+          ShareMenuReactView.continueInApp({ hello: "from the other side" });
+        }}
+      />
+      {sharedMimeType === "text/plain" && <Text>{sharedData}</Text>}
+      {sharedMimeType.startsWith("image/") && (
+        <Image source={{ uri: sharedData }} />
+      )}
+    </View>
+  );
+};
+
+AppRegistry.registerComponent("Test", () => Test);
+AppRegistry.registerComponent("ShareMenuModuleComponent", () => Share);
 
 
 export default App = () => {
@@ -63,6 +186,8 @@ export default App = () => {
           <Text style = {{color: "red"}}>Log Out</Text>
         </TouchableOpacity>
       </View>
+      <Test/>
+      
     </View>
   );
 }
